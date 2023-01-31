0e15a23e42313343631149e424adb2e04e06afd9
James Talbott <jltalbott47@gmail.com>
2023-01-31 10:18:55 -0800

    Add contamination-based thresholding
    Also corrected path length and anomaly score calculation
    
    Training now takes over 4 minutes rather than a couple seconds, will look into
    
     Changes to be committed:
    	modified:   test_IsolationForest.ipynb
    	modified:   turtleIsolationForests/isolationForest.py

--- turtleIsolationForests/isolationForest.py
+++ turtleIsolationForests/isolationForest.py
@@ -1,13 +1,16 @@
-import pandas as pd
+from heapq import nsmallest
 import math
+import pandas as pd
 import random
 
 class IsolationForest:
 
-    def __init__(self,                          
+    def __init__(self,
+                 contamination,                     #if integer, the number of expected anomalies. If float [0,1], the proportion of expected anomalies.
                  num_trees = 100,                   #default forest size as presented in original paper.
                  subsample_size = 256,              #default subsample size as presented in original paper.
                  random_state = None):
+        self.contamination = contamination
         self.subsample_size = subsample_size
         self.max_depth = math.log2(subsample_size)  #Maximum tree depth. After this depth, which is the average depth of all points, by definition points are not anomalous.
         self.num_trees = num_trees
@@ -32,27 +35,58 @@ class IsolationForest:
     
     def fit(self, train_data):                      #the whole dataset, as a dataframe, processed and suitable for training
         self.forest = [self.fit_tree(self.random_subsample(train_data)) for i in range(self.num_trees)]
-    
+        self.threshold = self.calculate_anomaly_score_threshold(train_data)
+
     def fit_tree(self, sample_data):
         point_to_isolate = self.random_point(sample_data)
         root = IsolationTree(sample_data)
         tree_pointer = root
-        while (tree_pointer.depth < self.max_depth and len(tree_pointer.data) > 1):
+        depth = 0
+        while (depth < self.max_depth and len(tree_pointer.data) > 1):
             tree_pointer.split()
             if (tree_pointer.decision.go_left(point_to_isolate)):
                 tree_pointer = tree_pointer.left
             else:
                 tree_pointer = tree_pointer.right
+            depth += 1
         return root
+    
+    def calculate_anomaly_score_threshold(self, train_data):
+        training_predictions = self.calculate_anomaly_scores(train_data)        #this takes aaaaages. how is sklearn doing it so fast, and do it during fit or predict?
+        contamination = self.normalize_contamination(train_data)
+        threshold_index = round(contamination * len(train_data))
+        if contamination <= 0.2:
+            threshold = nsmallest(threshold_index, training_predictions)[threshold_index]
+        else:
+            threshold = sorted(training_predictions)[threshold_index]
+        return threshold
 
     def predict(self, X_test):
         predictions = pd.DataFrame(index=X_test.index)
-        predictions['anomaly_score'] = X_test.apply(self.predict_point, axis=1, result_type='reduce')
+        predictions['anomaly_score'] = self.calculate_anomaly_scores(X_test)
+        predictions['predicted_as_anomaly'] = predictions['anomaly_score'] > self.threshold
         return predictions
     
-    def predict_point(self, point):
-        predictions = [tree.predict(point) for tree in self.forest]
-        return sum(predictions) / len(predictions)
+    def calculate_anomaly_scores(self, data):
+        self.len_data = len(data)
+        anomaly_scores = data.apply(self.calculate_anomaly_score, axis=1, result_type='reduce')
+        del self.len_data
+        return anomaly_scores
+    
+    def calculate_anomaly_score(self, point):
+        running_total = 0
+        count = 0
+        for tree in self.forest:
+            running_total += tree.path_length(point)
+            count += 1
+        path_length = running_total / count
+        return 2 ** (-1 * path_length / c(self.len_data))
+    
+    def normalize_contamination(self, data):        #accept int contaminations, but work internally only with float contaminations in [0,1]
+        if self.contamination is int:
+            return self.contamination / len(data)
+        else:
+            return min(1.0, max(0.0, self.contamination))
         
 
 class IsolationTree:
@@ -60,15 +94,12 @@ class IsolationTree:
     def __init__(self,
                  data,
                  parent = None):
-        self.data = data                            #the data at this node, before decision, if interior node.
+        self.data = data                            #the data at this node, before decision if interior node.
+        self.size = len(data)                       #the amount of data at this node, cached to reduce prediction calculations
         self.parent = None                          #parent Isolation Tree.
         self.left = None                            #left child Isolation Tree.
         self.right = None                           #right child Isolation Tree.
         self.decision = None                        #if interior node, the decision function, if leaf, None.
-        if parent is None:
-            self.depth = 0                          #with no parent, this tree is a root
-        else:
-            self.depth = parent.depth + 1           #depth of the tree, distance from root
         
     def split(self):
         attribute = random.sample(list(self.data.columns), 1)[0]
@@ -81,14 +112,16 @@ class IsolationTree:
         self.left = IsolationTree(left_data, parent = self)
         self.right = IsolationTree(right_data, parent = self)
     
-    def predict(self, point):
+    def path_length(self, point):
         tree_pointer = self
-        while (tree_pointer.decision is not None):  #None decision happens only at leaves
+        path_length = 0
+        while (tree_pointer.decision is not None):  #only leaves have None decision
+            path_length += 1
             if (tree_pointer.decision.go_left(point)):
                 tree_pointer = tree_pointer.left
             else:
                 tree_pointer = tree_pointer.right
-        return tree_pointer.depth
+        return path_length + c(tree_pointer.size) #As in IF paper, returned path length is adjusted up by c(|tree_data|)
 
 class Decision:
 
@@ -98,3 +131,17 @@ class Decision:
     
     def go_left(self, point_of_interest):
         return (point_of_interest[self.attribute] < self.value)
+
+def c(n):                                           #c(n) as defined in Isolation Forest Paper is the average path length
+                                                    #of an unsuccessful BST search and is used to normalize anomaly scores
+    if n > 2:
+        return 2 * harmonic_number(n - 1) - (2 * (n-1) / n)
+    elif n == 2:
+        return 1
+    else:
+        return 0
+    
+def harmonic_number(n):                             #this is an approximation that is valid for large n
+    #Euler-Mascheroni constant                      #see https://en.wikipedia.org/wiki/Harmonic_number
+    gamma = 0.5772156649015328606065120900824024310421
+    return math.log(n) + gamma
