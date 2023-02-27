d5bcd2fb0d9165cd7c7fe2216fc837a47d323eeb
Joshua Palicka <joshua.palicka@gmail.com>
2023-02-27 13:21:41 -0800

    Adds getZc()

--- autoencoder/aecExtraFeatures.py
+++ autoencoder/aecExtraFeatures.py
@@ -11,10 +11,20 @@ def get_Zcs(orig_sample, reconstr_sample):
     return dot(orig_sample[0], reconstr_sample[0]) / (norm(orig_sample[0]) * norm(reconstr_sample[0]))
 
 
-def getZc(orig_sample):  # TODO: Implement when we have MemAE implemented
-    return 0.0
+def getZc(encoded_sample):  # TODO: Implement when we have MemAE implemented
+    return encoded_sample
 
 
-def getZVector(orig_sample, reconstr_sample):
-    # does not return zc yet, as this is unable to be implemented yet
-    return [float(get_Zed(orig_sample, reconstr_sample)), float(get_Zcs(orig_sample, reconstr_sample))]
+def getZVector(orig_sample, reconstr_sample, encoded_sample):
+    zVector = []
+    #print("Encoded")
+    #print(encoded_sample)
+    for i in range(len(encoded_sample)):
+        zVector.append(float(encoded_sample[0][i]))
+    #zVector = [float(item) for item in encoded_sample]
+    zVector.append(float(get_Zed(orig_sample, reconstr_sample)))
+    zVector.append(float(get_Zcs(orig_sample, reconstr_sample)))
+    #print()
+    #print("Vector")
+    #print(zVector)
+    return zVector
