2815bd7ef181db5e3f2a54939a45ffce69300470
James Talbott <jltalbott47@gmail.com>
2023-03-15 11:37:17 -0700

    Forest testing notebook updates
     Changes to be committed:
    	modified:   pipelineUtils/runPipeline.py
    	modified:   test_FBIF_c1c2.ipynb
    	modified:   test_IF_auto.ipynb
    	modified:   test_IsolationForest.ipynb
    	new file:   test_minmax_forests.ipynb

--- pipelineUtils/runPipeline.py
+++ pipelineUtils/runPipeline.py
@@ -3,31 +3,19 @@ from pandas import DataFrame, Series
 from time import time
 from autoencoder.aecExtraFeatures import getZVector
 from turtleIsolationForests.printResults import calc_confusion, calc_f1, print_by_result, get_auroc_value
+from turtleIsolationForests.preprocessFeatures import minmax_preprocess_z_features
 import numpy as np
 import typing
 
-def run_pipeline(X_train: DataFrame, X_test: DataFrame, train_labels: DataFrame, test_labels: DataFrame, 
-                 autoenc: any, iForest: any, X_train_denoised: DataFrame = DataFrame(), intermediatePrint=False, printHistory=False, epochs=200):
+def run_pipeline(X_train: DataFrame, X_test: DataFrame, train_labels: DataFrame, test_labels: DataFrame, contamination: float,
+                 autoenc: any, iForest: any, X_train_ae: np.ndarray, intermediatePrint=False, epochs=600):
     train_labels_np = train_labels.to_numpy()
     test_labels_np = test_labels.to_numpy()
-    if X_train_denoised.empty:
-        X_train_ae = X_train[train_labels_np] #autoencoder trains only on normal data
-    else:
-        X_train_ae = X_train_denoised[train_labels_np] #or the denoised normal data from the robust autoencoder preprocessor
     history = autoenc.pipeline_fit(X_train_ae, epochs=epochs)
     start_time = time()
-    ae_scores, ae_predictions = autoenc.pipeline_predict(X_test, test_labels_np)
+    ae_scores, ae_predictions = autoenc.pipeline_predict(X_test, test_labels_np, contamination)
     ae_time = time() - start_time
     ae_TA, ae_FA, ae_FN, ae_TN = calc_confusion(ae_predictions, test_labels_np)
-    if printHistory:
-        plt.plot(history.history["loss"])
-        plt.plot(history.history["val_loss"])
-        plt.title("Training Loss & Validation Loss over epochs")
-        plt.xlabel("Epochs")
-        plt.ylabel("Loss")
-        plt.ylim([0, .08])
-        plt.legend(["Loss", "Validation Loss"])
-        plt.show()
     if intermediatePrint:
         ae_auroc = get_auroc_value(ae_scores, test_labels_np)
         ae_precision, ae_recall, ae_f1 = calc_f1(ae_TA, ae_FA, ae_FN, ae_TN)
@@ -36,9 +24,12 @@ def run_pipeline(X_train: DataFrame, X_test: DataFrame, train_labels: DataFrame,
         print("auroc: " + str(ae_auroc))
         print("test set prediction time: " + str(ae_time))
         print("")
+    pre_Z_cols = X_train.shape[1]
     X_train_forest = DataFrame(addZToData(X_train, autoenc))
-    iForest.fit(X_train_forest, train_labels)
+    minmax_preprocess_z_features(X_train_forest.iloc[:,pre_Z_cols:])
     X_test_forest = DataFrame(addZToData(X_test[ae_predictions], autoenc))
+    minmax_preprocess_z_features(X_test_forest.iloc[:,pre_Z_cols:])
+    iForest.fit(X_train_forest, train_labels)
     test_labels_forest_np = test_labels_np[ae_predictions]
     start_time = time()
     if_scores, if_predictions = iForest.predict(X_test_forest, test_labels_forest_np)
@@ -71,7 +62,7 @@ def addZToData(data: DataFrame, model) -> np.ndarray:
 
     data_with_Z = []
     for i in range(len(data)):
-        data_with_Z.append(addZToPoint(data[i:i+1],  encoded[i:i+1], decoded[i:i+1]))
+        data_with_Z.append(addZToPoint(data[i:i+1], encoded[i:i+1], decoded[i:i+1]))
 
     data_with_Z_np = np.stack(data_with_Z)
     return data_with_Z_np
